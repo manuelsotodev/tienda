b1368ab94fd4c52ba8163e6f04e1a65e8895d522
Manuel Yael Soto Baeza <47303594+manuelsotodev@users.noreply.github.com>
2021-12-09 00:46:42 -0600

    base de datos remota

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 //Exportar arreglo de configuracion para la conexion de DB
 module.exports= {
     database: {
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'tienda'
+        host:'mysql-manuel.alwaysdata.net',
+        user:'manuel',
+        password:'leones11',
+        database:'manuel_tienda'
     }
 }
